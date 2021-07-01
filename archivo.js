40d795751546091ac1313d6b0e6bde6c1ae98a08
Alexander Bedoya Espinel <alex22122020@gmail.com>
2021-07-01 16:46:37 -0500

    Agregado

--- archivo.js
+++ archivo.js
@@ -1,5 +1,6 @@
 
 alert("Esta es una calculadora de promedios para notas de materias universitarias, los estudiantes podran saber cuanto es el promedio total de su materia al tener las tres notas")
+
 function calcular(){
     let nombre = document.getElementById("nombre").value;
 
